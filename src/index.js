3df7d760a817204f97e4a6d0386525c169f369f0
Spencer Charest <SpencerC77@gmail.com>
2020-03-28 11:25:07 -0700

    Update Web Token

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class Tidal {
    */
   constructor(options = {}) {
     this.url = 'https://api.tidal.com/v1';
-    this.webToken = 'wdgaB1CilGA-S_s2';
+    this.webToken = 'kgsOOmYk3zShYrNP';
     this.countryCode = options.countryCode || 'US';
     this.limit = options.limit || 1000;
     this.api = axios.create({
